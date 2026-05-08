d32bb920f0ff62474262db69237373f53f99a626
max-t-wheeler <13712788+max-t-wheeler@users.noreply.github.com>
2026-05-08 02:14:35 -0700

    configure app to support static adapter build

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,18 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+    plugins: [tailwindcss(), sveltekit()],
+    build: {
+            rollupOptions: {
+                output:{
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        }
+                    }
+                }
+            }
+        }
+
+});
